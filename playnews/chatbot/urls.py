27969d5c3a839ad797e536ad530129b8d93969c6
heejin <huren091@naver.com>
2022-11-11 07:49:05 +0900

    v0.13

--- playnews/chatbot/urls.py
+++ playnews/chatbot/urls.py
@@ -5,5 +5,5 @@ from . import views
 # 패스에 따라 (url에 따라) 뷰 안의 챗봇이라는 함수를 호출함
 urlpatterns = [
     path('', views.chatbot, name='go_chatbot'),
-    #path('chat_service/', views.chat_service),
+    path('chat_service/', views.chat_service),
     ]
