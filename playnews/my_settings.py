72f71fbe85ae8d5bc65d1b4eff50ecf4996bc196
heejin <huren091@naver.com>
2022-12-08 19:03:28 +0900

    gitignore error

---
+++ playnews/my_settings.py
@@ -0,0 +1,11 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'playnewsdb',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
+SECRET_KEY = 'django-insecure-_pnaftv@x7*wexd=v@u=%z7xo21r=7d-=&fncni=4$$+%^4qgq'
